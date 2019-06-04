3f165eb283581d3e2ea54e25410401a4bfa9f9c4
Artiom Ciumac <v-arcium@microsoft.com>
2019-06-03 18:23:33 -0700

    Created an MVC route to Details action

--- BethanysPieShop.Web/Controllers/HomeController.cs
+++ BethanysPieShop.Web/Controllers/HomeController.cs
@@ -33,5 +33,17 @@ namespace BethanysPieShop.Web.Controllers
 
             return View(homeViewModel);
         }
+
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
     }
 }
